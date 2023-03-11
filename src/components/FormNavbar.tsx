41ac3459fd1e0e0f96d5476b75ea41723646ab8c
Kevin Nicolas <kncalderong@gmail.com>
2023-03-11 18:11:35 -0500

    fix: prevent skip form steps by navbar interaction

--- src/components/FormNavbar.tsx
+++ src/components/FormNavbar.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 const FormNavbar = () => {
-  const isFinhedthis = false
+
   return (
     <div className='flex gap-4'>
-      <NavLink className={({isActive})=>`w-[33px] h-[33px] rounded-full ${isActive ? 'bg-light-blue text-marine-blue ' : 'text-white border-[1px]'} flex justify-center items-center text-sm font-bold  `} to='/'>1</NavLink>
-      <NavLink className={({isActive})=>`w-[33px] h-[33px] rounded-full ${isActive ? 'bg-light-blue text-marine-blue ' : 'text-white border-[1px]'} flex justify-center items-center text-sm font-bold  ${isFinhedthis ? 'pointer-events-auto' : 'pointer-events-none' }  `}  to='/select-plan'>2</NavLink>
-      <NavLink className={({isActive})=>`w-[33px] h-[33px] rounded-full ${isActive ? 'bg-light-blue text-marine-blue ' : 'text-white border-[1px]'} flex justify-center items-center text-sm font-bold  `}  to='/adds-on'>3</NavLink>
-      <NavLink className={({isActive})=>`w-[33px] h-[33px] rounded-full ${isActive ? 'bg-light-blue text-marine-blue ' : 'text-white border-[1px]'} flex justify-center items-center text-sm font-bold  `}   to='/summary'>4</NavLink>
+      <NavLink className={({isActive})=>`w-[33px] h-[33px] rounded-full ${isActive ? 'bg-light-blue text-marine-blue ' : 'text-white border-[1px]'} flex justify-center items-center text-sm font-bold pointer-events-none `} to='/'>1</NavLink>
+      <NavLink className={({isActive})=>`w-[33px] h-[33px] rounded-full ${isActive ? 'bg-light-blue text-marine-blue ' : 'text-white border-[1px]'} flex justify-center items-center text-sm font-bold pointer-events-none`}  to='/select-plan'>2</NavLink>
+      <NavLink className={({isActive})=>`w-[33px] h-[33px] rounded-full ${isActive ? 'bg-light-blue text-marine-blue ' : 'text-white border-[1px]'} flex justify-center items-center text-sm font-bold pointer-events-none `}  to='/adds-on'>3</NavLink>
+      <NavLink className={({isActive})=>`w-[33px] h-[33px] rounded-full ${isActive ? 'bg-light-blue text-marine-blue ' : 'text-white border-[1px]'} flex justify-center items-center text-sm font-bold pointer-events-none `}   to='/summary'>4</NavLink>
     </div>
   )
 }
